7689ca9f6499b95be5aa22027940a4354d270d27
Danail Branekov <danailster@gmail.com>
2019-10-23 12:13:22 +0300

    First K8s operator
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

---
+++ pkg/controller/add_podset.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"first-operator/first-operator/pkg/controller/podset"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, podset.Add)
+}
